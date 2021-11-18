6b5a44bf74e006cabcba33fab97ddb4b0c94c9fb
mr2981 <36012529+mr2981@users.noreply.github.com>
2021-11-18 17:35:52 +0000

    Create DiceTest.java

---
+++ src/test/java/com/mr2981/SnakeAndLadderGame/DiceTest.java
@@ -0,0 +1,35 @@
+package com.mr2981.SnakeAndLadderGame;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+import com.mr2981.SnakeAndLadderGame.entity.Dice;
+
+public class DiceTest {
+	private Dice dice;
+
+    @BeforeAll
+    public void setup() {
+        dice = new Dice();
+    }
+
+    @Test
+    public void itShouldRollAndGenerateRandomNumberBetweenOneAndSixIncluded() {
+        // given
+        // when
+        int currRoll = dice.roll();
+        List<Integer> validStates = Arrays.asList(1,2,3,4,5,6);
+
+        // then
+        Assertions.assertTrue(currRoll > 0);
+        Assertions.assertTrue(currRoll < 7);
+        Assertions.assertTrue(validStates.contains(currRoll));
+    }
+
+
+}
