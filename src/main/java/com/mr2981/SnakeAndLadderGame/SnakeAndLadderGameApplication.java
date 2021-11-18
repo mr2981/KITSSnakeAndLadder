f82eea2f18eaa59caea0644d85514e7d11900ad3
mr2981 <36012529+mr2981@users.noreply.github.com>
2021-11-18 17:33:40 +0000

    Add files via upload

---
+++ src/main/java/com/mr2981/SnakeAndLadderGame/SnakeAndLadderGameApplication.java
@@ -0,0 +1,13 @@
+package com.mr2981.SnakeAndLadderGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakeAndLadderGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnakeAndLadderGameApplication.class, args);
+	}
+
+}
