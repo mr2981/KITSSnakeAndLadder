16dd1eb51377deaa903d8daca386f06b0fe418e3
mr2981 <36012529+mr2981@users.noreply.github.com>
2021-11-19 10:07:40 +0000

    Update LadderTest.java

--- src/test/java/com/mr2981/SnakeAndLadderGame/LadderTest.java
+++ src/test/java/com/mr2981/SnakeAndLadderGame/LadderTest.java
@@ -1,7 +1,7 @@
 package com.mr2981.SnakeAndLadderGame;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.mr2981.SnakeAndLadderGame.entity.Ladder;
@@ -10,7 +10,7 @@ import com.mr2981.SnakeAndLadderGame.entity.Tile;
 public class LadderTest {
 	 private Ladder ladder;
 
-	    @BeforeAll
+	    @BeforeEach
 	    public void setup() {
 	        ladder = new Ladder(23);
 	    }
